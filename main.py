d89f9546d3d0f40223de1368e526f41889926c9f
Arin Bryan <arinbryan27@gmail.com>
2020-01-16 13:41:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+number=int(input("Please enter a number"))
+if number % 3 == 0 and number % 5 == 0:
+  print("FizzBuzz")
+else:
+  if number % 3 == 0:
+    print("Fizz")
+  if number % 5 == 0:
+    print("Buzz")
+
+
+
+
